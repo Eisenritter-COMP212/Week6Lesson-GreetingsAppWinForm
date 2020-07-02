1291f580e0fc0aeba11542024120001f9a785a39
EisenritterX <georgezhou89@hotmail.com>
2020-07-02 18:06:41 -0400

    Initial Commit

--- GreetingsAppWinForm/Form1.Designer.cs
+++ GreetingsAppWinForm/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNameHere = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Greeting App";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(291, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 72);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hello";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNameHere
+            // 
+            this.txtNameHere.Location = new System.Drawing.Point(84, 93);
+            this.txtNameHere.Name = "txtNameHere";
+            this.txtNameHere.Size = new System.Drawing.Size(495, 26);
+            this.txtNameHere.TabIndex = 2;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(84, 351);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(170, 20);
+            this.lblOutput.TabIndex = 3;
+            this.lblOutput.Text = "Output Displayed Here";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtNameHere);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNameHere;
+        private System.Windows.Forms.Label lblOutput;
     }
 }
 
--- GreetingsAppWinForm/Form1.cs
+++ GreetingsAppWinForm/Form1.cs
@@ -16,5 +16,10 @@ namespace GreetingsAppWinForm
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblOutput.Text = $"Welcome {txtNameHere.Text}";
+        }
     }
 }
